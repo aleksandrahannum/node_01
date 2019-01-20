1779a3d595b8f3711a4bb616ebb18c6790fcf5e8
Aleksandra Hannum <aleksandrahannum@gmail.com>
2019-01-20 17:57:48 +0100

    update

--- node_01/app.js
+++ node_01/app.js
@@ -16,25 +16,71 @@ const FS = require('fs');
 // console.log(filesInDirectory);
 const arrayOfimages = []
 const filesFolder = './img'
+const myReadFile = () => {
+    return new Promise((resolve, reject) => {
+        FS.readdir(filesFolder, (err, files) => {
+            if (err) {
+                reject(err)
+                
+            } else {
+                resolve(files)
+            }
+        })
+    })
+}
 
-const exampleFileText = FS.readdir(filesFolder, (err, files) => {
-    if (err && !files) {
-        console.error(err)
-        
-    }
-    
-
+myReadFile()
+    .then(files => {
     let regexp = RegExp('.jpg');
-
     files.forEach(file => {
-        
-        if(file.match(regexp)) {
-            // arrayOfimages.push(file)
-            console.log(file)
-            
-    
+        let isDirectory = FS.statSync(file).isDirectory()
+        if(file.match(regexp) && isDirectory === false) {
+            arrayOfimages.push(file)
         }
     })
-  })
-console.log(arrayOfimages)
-console.log(exampleFileText)
+}, console.log(arrayOfimages))
+    .catch(err => {
+    console.error(err)
+})
+
+
+// myReadFile()
+//     .then(files => {
+//       let regexp = RegExp('.jpg');
+//       files.forEach(file => {
+//         let isDirectory = FS.statSync(file).isDirectory();
+//         if(file.match(regexp) && isDirectory === false) {
+//             arrayOfimages.push(file)
+//         }
+//       })
+//       console.log(arrayOfimages);
+//     }).catch(err => {
+//       console.log(err)
+//     })
+
+
+
+// console.log(arrayOfimages)
+
+// const exampleFileText = FS.readdir(filesFolder, (err, files) => {
+//     if (err && !files) {
+//         console.error(err)
+        
+//     }
+    
+
+//     let regexp = RegExp('.jpg');
+
+//     files.forEach(file => {
+        
+//         if(file.match(regexp)) {
+//             // arrayOfimages.push(file)
+//             console.log(file)
+            
+    
+//         }
+//     })
+//     console.log(arrayOfimages)
+//     console.log(exampleFileText)
+//   })
+
